51fd7d6c650e504dacc5aca17c054803564b7e27
Prajwol <96991442+PrajwolNeupane@users.noreply.github.com>
2023-02-23 13:42:49 +0000

    Hate Yougit add .

--- src/Pages/FirstPage.js
+++ src/Pages/FirstPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import { Stack, Typography } from '@mui/material';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import first from '../images/first.png';
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -14,21 +15,20 @@ export default function FirstPage() {
         gsap.to(TitleRef.current, {
             fontSize: "200px",
             opacity:1,
-            duration: 1,
+            duration: .5,
             scrollTrigger: {
-                toggleActions: "repeat none none none",
+                toggleActions: "play reverse play reverse",
                 trigger: TitleRef.current,
-                start: "center 90%",
-                end: "center 10%",
-                markers: true,
-                scrub:1
+                start: "center 95%",
+                end: "center 5%",
             }
         });
     }, []);
 
     return (
-        <Stack sx={{ alignItems: "center", height: "500vh", justifyContent: "flex-start",overflow:"hidden" }}>
-            <Typography variant='ken'  sx={{ color: "black", opacity:1,textAlign: "center", fontSize: "2000px", lineHeight: "90%",width:"1000%" }}>WELCOME TO <br />GAME</Typography>
+        <Stack sx={{ alignItems: "center", height: "100vh", justifyContent: "center",backgroundColor:"text.main",overflow:"hidden" }}>
+            <Typography variant='ken' sx={{fontSize:"250px",color:"primary.main",position:"absolute",left:"-1.5em",transform: "rotate(90deg)",overflow:"hidden" }}>G2 ESPORT</Typography>
+            <img src={first} style={{width:"100%",height:"100%",objectFit:"cover",zIndex:1}}/>
         </Stack>
     )
 }
